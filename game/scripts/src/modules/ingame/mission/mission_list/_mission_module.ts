7b0817264bdb244bae2b7b6c0da2a579360a70b9
guoyuan <100774474@qq.com>
2024-08-22 22:25:10 +0800

    任务空白模块

---
+++ game/scripts/src/modules/ingame/mission/mission_list/_mission_module.ts
@@ -0,0 +1,19 @@
+
+
+/** 任务基础模版 */
+export class MissionModule {
+
+    /** 初始化d当前配置 */
+    _InitEventConfig() {
+
+    }
+
+    /**
+     * 发放奖励
+     * @param success 成功/失败
+     */
+
+    GiveRewards(success: boolean) {
+
+    }
+}
