34c3a4a8a56e318876b87a4178ae6cd13a16b1e9
guoyuan <100774474@qq.com>
2024-10-09 00:02:33 +0800

    击飞方法

--- game/scripts/src/modules/ingame/public/entity_killed.ts
+++ game/scripts/src/modules/ingame/public/entity_killed.ts
@@ -58,4 +58,52 @@ export class EntityKilled {
     // ArmsKillAbility(hAttacker: CDOTA_BaseNPC, hDeathUnit: CDOTA_BaseNPC, hAbility: CDOTA_BaseNPC) {
     //     // 技能击杀
     // }
+
+    /** 死亡击飞 */
+    PlayDeathAnimation(me: CDOTA_BaseNPC, hAttacker: CDOTA_BaseNPC) {
+        let knockback_duration = 0.5;
+        let knockback_distance = RandomInt(300, 600);
+        let knockback_height = RandomInt(100, 200);
+        let fGroundSpeed = knockback_distance / knockback_duration
+        let attack_vect = hAttacker.GetOrigin();
+        let unit_origin = me.GetAbsOrigin()
+        let vDir = (unit_origin - attack_vect as Vector).Normalized()
+        let vFinal = unit_origin + vDir * knockback_distance as Vector;
+        let dt = 0.03
+        let moved_distance = fGroundSpeed * dt;
+        let fMovedDistance = 0;
+        let fMovedHeigh = 0;
+        let height_start = GetGroundHeight(unit_origin, me);
+        let height_end = GetGroundHeight(vFinal, me);
+        let height_max = height_start + knockback_height;
+        height_end = height_end - height_start;
+        height_max = height_max - height_start;
+        if (height_max < height_end) { height_max = height_end + 0.01; }
+        if (height_max <= 0) { height_max = 0.01; }
+        let duration_end = (1 + math.sqrt(1 - height_end / height_max)) / 2;
+        let fHeightConst1 = 4 * height_max * duration_end / knockback_duration;
+        let fHeightConst2 = 4 * height_max * duration_end * duration_end / (knockback_duration * knockback_duration);
+        let time = 0;
+        me.SetContextThink("death_anima", () => {
+            // let origin = hUnit.GetAbsOrigin();
+            let speed = (fHeightConst1 - 2 * fHeightConst2 * time) * dt;
+            let vNewLocation = me.GetOrigin() + vDir * fGroundSpeed * dt as Vector;
+            vNewLocation.z += speed
+            // 高度
+            me.SetOrigin(vNewLocation);
+            let ground = GetGroundHeight(vNewLocation, me);
+            if (vNewLocation.z <= ground) {
+                vNewLocation.z = ground;
+                me.SetOrigin(vNewLocation);
+                return null;
+            }
+            fMovedDistance += moved_distance;
+            if (fMovedDistance >= knockback_distance) {
+                return null;
+            }
+
+            time += dt
+            return dt
+        }, 0)
+    }
 }
